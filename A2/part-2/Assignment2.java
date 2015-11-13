42033389bca2dd7659c6d6ea92970c9df3e766cd
g5javier <g5javier@da5a67a5-88e4-45ca-a5b3-6c52652e6944>
2015-11-13 07:48:40 +0000

    separated statements

--- A2/part-2/Assignment2.java
+++ A2/part-2/Assignment2.java
@@ -79,7 +79,7 @@ public class Assignment2 {
 
 			queryString = "CREATE VIEW GenreMatch AS SELECT Genre.genre_id, genre FROM Genre, Album WHERE Genre.genre_id = Album.genre_id;" +
 			"CREATE VIEW ArtistAlbum AS SELECT name, genre_id FROM Artist, Album WHERE Artist.artist_id = Album.artist_id;" +
-			"SELECT DISTINCT name FROM GenreMatch, ArtistAlbum WHERE ArtistAlbum.genre_id = GenreMatch.genre_id and GenreMatch.genre =" + genre;
+			"SELECT DISTINCT name FROM GenreMatch, ArtistAlbum WHERE ArtistAlbum.genre_id = GenreMatch.genre_id and GenreMatch.genre = \"" + genre + "\"";
 
 			pStatement = connection.prepareStatement(queryString);
 			rs = pStatement.executeQuery();
