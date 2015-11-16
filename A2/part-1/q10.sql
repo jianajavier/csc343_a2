d2fe861058eb91f1a4b4fb9e6f7ffa88068cc597
c5chenjk <c5chenjk@da5a67a5-88e4-45ca-a5b3-6c52652e6944>
2015-11-16 03:12:05 +0000

    Everything works

--- A2/part-1/q10.sql
+++ A2/part-1/q10.sql
@@ -16,16 +16,18 @@ WHERE song_id in (SELECT song_id FROM SongsFromThriller);
 DELETE FROM ProducedBy
 WHERE album_id in (SELECT album_id FROM AlbumThriller);
 
+
 DELETE FROM BelongsToAlbum WHERE album_id IN
+(SELECT album_id FROM AlbumThriller);
+
+DELETE FROM Album WHERE album_id IN
 (SELECT album_id FROM AlbumThriller);
 
 DELETE FROM Song AS t1 WHERE t1.song_id IN 
 (SELECT song_id FROM SongsFromThriller);
 		  
-DELETE FROM Album WHERE album_id IN
-(SELECT album_id FROM SongsFromThriller);
 
 DROP VIEW AlbumThriller CASCADE;
-
+DROP TABLE SongsFromThriller;
 
 
